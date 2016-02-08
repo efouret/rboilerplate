a2d3f87fe08025ad8d2a184b87fd930b56856328
Erwan <erwan.fouret@gmail.com>
2016-02-08 22:07:45 +0100

    Added project component

---
+++ web/components/Project.jsx
@@ -0,0 +1,11 @@
+const React = require('react');
+
+const Project = React.createClass({
+   render() {
+       return (
+         <div>TO DO</div>  
+       );
+   } 
+});
+
+module.exports = Project;
